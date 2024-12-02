09e0276b7819507b82c49f6d5fee9342c582e884
Nagesh Kurra <kurra.nani1@gmail.com>
2024-12-02 21:14:14 +0800

    removed sen data

--- Desktop/TerraformAzure/main.tf
+++ Desktop/TerraformAzure/main.tf
@@ -3,7 +3,7 @@ provider "azurerm" {
     subscription_id = "b288bb52-5daf-4140-996e-bc695ea56123"
     client_id = "7a4c018d-c01e-4863-b69c-386145f7a448"
     tenant_id = "4cee185a-29d4-460b-a308-4f111579e2a1"
-    client_secret = "-vD8Q~SUtaqv4AJFG3DPghl6gLkRDs.SoJvRScgR"
+    client_secret = "var.secret"
  }
 resource "azurerm_resource_group" "firstresource" {
     name = "Testapplication"
